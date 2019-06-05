504097aebe2048558236d2fe47d258f4843fda44
morteza behboodian <xeptore@gmail.com>
2019-06-05 14:09:20 +0430

    add borrow book to member capability

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ void help();
 int getaction();
 
 Member *addNewMember(Store *store);
+void borrowBookToMember(Store *store);
 
 int main()
 {
@@ -27,6 +28,8 @@ int main()
         error("Just enter number of the option you want.");
         help();
     }
+
+    println();
 
     switch (action)
     {
@@ -52,6 +55,7 @@ int main()
     }
     case 3:
     {
+        borrowBookToMember(store);
     }
     default:
         break;
@@ -104,7 +108,6 @@ Member *addNewMember(Store *store)
     string firstName, lastName, id;
     uint entranceYear;
 
-    println();
     info("Enter new member information:");
 
     firstName = promptString("First Name:");
@@ -121,4 +124,49 @@ Member *addNewMember(Store *store)
     store->addMember(member);
 
     return member;
+}
+
+void borrowBookToMember(Store *store)
+{
+    auto isbn = promptString("Enter book ISBN:");
+    auto book = store->findBookByISBN(isbn);
+    if (book == NULL)
+    {
+        error("Could not find book with entered ISBN.");
+        return;
+    }
+
+    if (!book->canBeBorrowed())
+    {
+        error("All books have been borrowed.");
+        return;
+    }
+
+    auto memberId = promptString("Enter member ID:");
+    auto member = store->findMemberById(memberId);
+    if (member == NULL)
+    {
+        error("Member with entered ID is not registerd.");
+        return;
+    }
+
+    if (!book->canBeBorrowedTo(member))
+    {
+        error("Member has already borrowed this book.");
+        return;
+    }
+
+    book->borrowTo(member);
+
+    ok("Book with ISBN (" + book->getISBN() + ")");
+    ok("has been successfully borrowed to member (" + member->getId() + ")");
+    string s = "";
+    if (book->getRemainingCount() > 1)
+    {
+        s = "s";
+    }
+
+    ok("number of remaining unborrowed book" + s + ": " + to_string(book->getRemainingCount()));
+
+    return;
 }
