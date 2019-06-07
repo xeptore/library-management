339dffe6b03759278f38378ee66a261caa89d2e2
morteza behboodian <xeptore@gmail.com>
2019-06-07 07:58:47 +0430

    code style

--- include/member.h
+++ include/member.h
@@ -8,29 +8,29 @@ class Book;
 
 class Member
 {
-  private:
-    string firstName;
-    string lastName;
-    uint entranceYear;
-    string id;
-    vector<Book*> borrowedBooks;
+private:
+  string firstName;
+  string lastName;
+  uint entranceYear;
+  string id;
+  vector<Book *> borrowedBooks;
 
-  public:
-    Member(string id);
-    ~Member();
+public:
+  Member(string id);
+  ~Member();
 
-    string getFirstName();
-    string getLastName();
-    uint getEntranceYear();
-    string getId();
-    vector<Book*> getBorrowedBooks();
-    uint getBorrowedBooksCount();
-    bool hasBorrowedBook(Book *book);
+  string getFirstName();
+  string getLastName();
+  uint getEntranceYear();
+  string getId();
+  vector<Book *> getBorrowedBooks();
+  uint getBorrowedBooksCount();
+  bool hasBorrowedBook(Book *book);
 
-    Member *setFirstName(string firstName);
-    Member *setLastName(string lastName);
-    Member *setEntranceYear(uint entranceYear);
-    Member *setId(string id);
-    uint addBorrowedBook(Book *book);
-    bool removeBorrowedBook(Book *book);
+  Member *setFirstName(string firstName);
+  Member *setLastName(string lastName);
+  Member *setEntranceYear(uint entranceYear);
+  Member *setId(string id);
+  uint addBorrowedBook(Book *book);
+  bool removeBorrowedBook(Book *book);
 };
